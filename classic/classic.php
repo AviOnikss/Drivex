003945714db9f6481cfbf488d6d86902c461869a
Onaliy Vinukiy <114973581+Onaliy@users.noreply.github.com>
2023-06-26 13:02:58 +0530

    new files

---
+++ classic/classic.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Classic Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/luxury.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+<style>
+    <?php include "../styles/luxury.css"?>
+    <?php include "../styles/about_responsive.css"?>
+@media screen and (max-width: 992px) {
+  .portfolio-block{
+    width:280px;
+    
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .portfolio-block {
+    width: 280px;
+  }
+  }
+@media screen and (max-width: 992px) {
+  .img-responsive{
+    width: 100px;
+    height : 100px;
+  }
+}
+
+
+@media screen and (max-width: 600px) {
+  .img-responsive {
+    width: 100px;
+    height:100px
+}
+}
+    </style>
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+
+
+		<!-- Main Navigation -->
+
+		
+		<nav class="main_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col main_nav_col d-flex flex-row align-items-center justify-content-start">
+						<div class="logo_container">
+							<div class="logo"><img src="../image/Drivex.png"></div>
+						</div>
+						<div class="main_nav_container ml-auto">
+							<ul class="main_nav_list">
+								<li class="main_nav_item"><a href="../index.php">home</a></li>
+								<li class="main_nav_item"><a href="../about.php">about us</a></li>
+								<li class="main_nav_item"><a href="../blog.php">blog</a></li>
+								<li class="main_nav_item"><a href="../index.php#contact">Our Team</a></li>
+								<li class="main_nav_item"><a href="../contact.php">contact us</a></li>
+							</ul>
+						</div>
+						<div >
+							
+								<g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
+											s112.344-22.55,153.287-63.494C489.451,329.123,512,274.686,512,216.78c0-57.904-22.549-112.342-63.494-153.286
+											C407.563,22.549,353.124,0,295.219,0c-57.904,0-112.342,22.549-153.287,63.494C100.988,104.438,78.439,158.876,78.438,216.78z
+											M119.804,216.78c0-96.725,78.69-175.416,175.415-175.416s175.418,78.691,175.418,175.416
+											c0,96.725-78.691,175.416-175.416,175.416C198.495,392.195,119.804,313.505,119.804,216.78z"/>
+										</g>
+									</g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
+											c8.076-8.076,8.076-21.172,0-29.248c-8.076-8.078-21.172-8.078-29.249,0L6.057,476.693
+											C-2.019,484.77-2.019,497.865,6.057,505.942z"/>
+										</g>
+									</g>
+								</g>
+							</svg>
+						</div>
+
+						
+
+						<div class="hamburger">
+							<i class="fa fa-bars trans_200"></i>
+						</div>
+					</div>
+				</div>
+			</div>	
+		
+		</nav>
+
+	</header>
+
+	<div class="menu trans_500">
+		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+			<div class="menu_close_container"><div class="menu_close"></div></div>
+
+		
+
+			<div class="logo menu_logo"><img src="../image/Drivex.png" ></div>
+
+			<ul>
+				<li class="menu_item"><a href="../index.php">home</a></li>
+				<li class="menu_item"><a href="../about.php">about us</a></li>
+				<li class="menu_item"><a href="../blog.php">blog</a></li>
+				<li class="menu_item"><a href="../index.php#contact">Feedback</a></li>
+				<li class="menu_item"><a href="../contact.php">contact us</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+	
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Classic Car Brands</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					
+				
+
+			</div>
+		</div>
+	</div>
+
+
+	
+
+
+	<div class="row portfolio-items-wrapper">
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/benz.png" alt="">
+                <div class="caption">
+
+                    <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/mercedclas.php" data-lightbox="../image-1">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <h4><a href="../classic/mercedclas.php">Mercedes Benz</a></h4>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/bmw.png"  alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/bmwclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/bmwclas.php">BMW</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design development">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/nissan.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/nissanclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/nissanclas.php">Nissan</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 photography development">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/mini.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/minicoopclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/minicoopclas.php">Mini Cooper</a></h4>
+                    
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 photography development">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/mit.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/mitshuclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/mitshuclas.php">Mitsubishi</a></h4>
+                    
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 photography" >
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/porche.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/porscheclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/porscheclas.php">Porsche</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/jaguar.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/jaguarclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/jaguarclas.php">Jaguar</a></h4>
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/ferrari.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/ferrariclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/ferrariclas.php">Ferrari</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/Chevorlt.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon ../image-popup" data-effect="mfp-with-zoom" href="../classic/chevoclas.php" data-lightbox="../image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="../classic/chevoclas.php">Chevrolet</a></h4>
+                    </div>
+                </div>
+        </div>
+        
+             
+            </div>
+        </div>
+                                            
+    </div>
+        
+
+	
+
+	
+	
+	<!-- Footer -->
+	
+	<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+</body>
+
+</html>
